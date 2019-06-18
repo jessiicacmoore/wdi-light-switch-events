aaaa2103cda01620a3b5c89df1ae55c0c6c6a7f6
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-18 12:59:02 -0400

    Complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+const lightSwitch = document.querySelector('.switch');
+const body = document.querySelector('body');
+const lightStatus = document.querySelector('.status')
 
+
+lightSwitch.addEventListener('click', () => {
+  if (lightSwitch.classList.contains('on')) {
+    lightSwitch.classList.remove('on');
+    lightSwitch.classList.add('off');
+    body.classList.remove('light');
+    body.classList.add('dark');
+    lightStatus.innerText = "Hey, who turned off the lights?"
+  } else {
+    lightSwitch.classList.remove('off');
+    lightSwitch.classList.add('on');
+    body.classList.remove('dark');
+    body.classList.add('light');
+    lightStatus.innerText = "It's so bright in here!"
+  }
+});
 
